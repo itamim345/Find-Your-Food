d89902e0ade56976522952433191991deadd1b1e
AF Tamim <itamim345@gmail.com>
2022-06-28 09:50:28 +0530

    applied js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById('button-addon2').addEventListener('click', () => {
+    let inputValue = document.getElementById('input-filed').value;
+    let url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
+    fetch(url)
+    .then (resp => resp.json())
+    .then(data => displayFood(data.meals))
+})
+
+function displayFood (foods) {
+    console.log(foods);
+    let wrapper =  document.getElementById('show-foods');
+    for (let food of foods) {
+       let div = document.createElement('div');
+       div.classList.add('col');
+       div.innerHTML = `
+                        <div class="card">
+                            <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">${food.strMeal}</h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Ingredients:- ${food.strIngredient1 + ", " +  food.strIngredient2 +", " +  food.strIngredient3+", " +  food.strIngredient4+", " +  food.strIngredient5+", " +  food.strIngredient6+", " +  food.strIngredient7+", " +  food.strIngredient8+", " +  food.strIngredient9+", " +  food.strIngredient10 +" and Etc.."} </li>
+                                <li class="list-group-item">Place of origin:- ${food.strArea}</li>
+                                <li class="list-group-item">Food Catagory:- ${food.strCategory}</li>
+                            </ul>
+                        </div>  
+                    `
+        wrapper.appendChild(div);
+    }
+}
